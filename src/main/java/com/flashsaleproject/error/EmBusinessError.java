90c7523a2cee95b77c3f991b398b5825a2a8e608
Mohamed Ayadi <151476140+Mayedi007@users.noreply.github.com>
2025-04-13 17:17:07 +0100

    Update EmBusinessError.java

--- src/main/java/com/flashsaleproject/error/EmBusinessError.java
+++ src/main/java/com/flashsaleproject/error/EmBusinessError.java
@@ -1,26 +1,34 @@
 package com.flashsaleproject.error;
 
-public enum EmBusinessError implements CommonError{
-    //通用错误类型10001
-    PARAMETER_VALIDATION_ERROR(10001,"Parameter does not validate"),
-    UNKNOWN_ERROR(10002,"Unknown error"),
-    //20000开头为用户信息相关错误
-    USER_NOT_EXIST(20001,"User does not exsit"),
-    USER_LOGIN_FAIF(20002,"User telephone number or password is not correct."),
-    USER_NOT_LOGIN(20003,"User have not login"),
+/**
+ * Enumeration of business error types used across the application.
+ * Implements the CommonError interface for uniform error handling.
+ *
+ * Author: Mohamed Ayadi
+ * GitHub: https://github.com/Mayedi007
+ * Date: 2025-04-13
+ */
+public enum EmBusinessError implements CommonError {
 
-    //30000订单相关错误
-    STOCK_NOT_ENOUGH(30001, "Item stock is not enough")
-    ;
+    // 10000 - General errors
+    PARAMETER_VALIDATION_ERROR(10001, "Parameter does not validate"),
+    UNKNOWN_ERROR(10002, "Unknown error"),
 
-    private int errCode;
+    // 20000 - User-related errors
+    USER_NOT_EXIST(20001, "User does not exist"),
+    USER_LOGIN_FAIL(20002, "User telephone number or password is not correct"),
+    USER_NOT_LOGIN(20003, "User is not logged in"),
+
+    // 30000 - Order-related errors
+    STOCK_NOT_ENOUGH(30001, "Item stock is not enough");
+
+    private final int errCode;
     private String errMsg;
 
-    private EmBusinessError(int errCode, String errMsg) {
+    EmBusinessError(int errCode, String errMsg) {
         this.errCode = errCode;
         this.errMsg = errMsg;
     }
-
 
     @Override
     public int getErrCode() {
@@ -35,6 +43,3 @@ public enum EmBusinessError implements CommonError{
     @Override
     public CommonError setErrMsg(String errMsg) {
         this.errMsg = errMsg;
-        return this;
-    }
-}
