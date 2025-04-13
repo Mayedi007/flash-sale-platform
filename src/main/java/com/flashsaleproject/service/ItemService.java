0508f409682a73cd9c209d3d402a78dd491d2b38
Mohamed Ayadi <151476140+Mayedi007@users.noreply.github.com>
2025-04-13 17:19:21 +0100

    Update ItemService.java

--- src/main/java/com/flashsaleproject/service/ItemService.java
+++ src/main/java/com/flashsaleproject/service/ItemService.java
@@ -5,22 +5,55 @@ import com.flashsaleproject.service.model.ItemModel;
 
 import java.util.List;
 
+/**
+ * Service interface for item management in the flash sale platform.
+ *
+ * Author: Mohamed Ayadi
+ * GitHub: https://github.com/Mayedi007
+ * Date: 2025-04-13
+ */
 public interface ItemService {
 
-    //创建商品
-    ItemModel creatItem(ItemModel itemModel) throws BusinessException;
+    /**
+     * Create a new item.
+     *
+     * @param itemModel the item data
+     * @return the created item
+     * @throws BusinessException in case of validation or business logic errors
+     */
+    ItemModel createItem(ItemModel itemModel) throws BusinessException;
 
-    //商品列表浏览
+    /**
+     * Retrieve the list of all available items.
+     *
+     * @return list of item models
+     */
     List<ItemModel> listItem();
 
-    //商品详情浏览
+    /**
+     * Retrieve item details by ID.
+     *
+     * @param id the item ID
+     * @return the corresponding item model
+     */
     ItemModel getItemById(Integer id);
 
-    //减库存
+    /**
+     * Decrease item stock.
+     *
+     * @param itemId the item ID
+     * @param amount quantity to decrease
+     * @return true if successful, false if not enough stock
+     * @throws BusinessException if stock cannot be decreased
+     */
     Boolean decreaseStock(Integer itemId, Integer amount) throws BusinessException;
 
-    //增销量
+    /**
+     * Increase item sales count.
+     *
+     * @param itemId the item ID
+     * @param amount quantity to increase
+     * @throws BusinessException in case of business logic failure
+     */
     void increaseSales(Integer itemId, Integer amount) throws BusinessException;
-
-
 }
