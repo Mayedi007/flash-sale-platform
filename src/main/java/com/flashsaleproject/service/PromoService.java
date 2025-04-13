8afea410351ca65b30e226ac1ef70af8b205e57d
Mohamed Ayadi <151476140+Mayedi007@users.noreply.github.com>
2025-04-13 17:20:12 +0100

    Update PromoService.java

--- src/main/java/com/flashsaleproject/service/PromoService.java
+++ src/main/java/com/flashsaleproject/service/PromoService.java
@@ -2,9 +2,19 @@ package com.flashsaleproject.service;
 
 import com.flashsaleproject.service.model.PromoModel;
 
+/**
+ * Service interface for managing promotional flash sale events.
+ *
+ * Author: Mohamed Ayadi
+ * GitHub: https://github.com/Mayedi007
+ */
 public interface PromoService {
 
-
-    //根据itemid获取即将开始或正在进行的秒杀活动
+    /**
+     * Retrieve the promo event associated with a given item, if any.
+     *
+     * @param itemId the item ID
+     * @return the associated PromoModel, or null if none
+     */
     PromoModel getPromoByItemId(Integer itemId);
 }
